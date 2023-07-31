d4cd07b665c8e7999edf757cc74839b1b65253ac
Dan Lopez <dlp@oiga.com>
2023-07-31 08:42:27 -0500

    feat: create example app to integrate other one as a micro app

---
+++ lib/src/settings/settings_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_blockchain_poc/main.dart';
+import 'package:flutter/material.dart';
+
+import 'settings_controller.dart';
+
+/// Displays the various settings that can be customized by the user.
+///
+/// When a user changes a setting, the SettingsController is updated and
+/// Widgets that listen to the SettingsController are rebuilt.
+class SettingsView extends StatelessWidget {
+  const SettingsView({super.key, required this.controller});
+
+  static const routeName = '/settings';
+
+  final SettingsController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return const BlockchainApp();
+  }
+}
